55608f861796c5e8594574903fd9aabcc780c82f
Sabina Rusu <rususabina0@gmail.com>
2023-01-13 15:06:00 +0200

    added operators js

---
+++ pb1.js
@@ -0,0 +1,24 @@
+//1.  2 + 3 x 4 - 5
+
+const rez1 = ( 2 + 3 * 4 - 5);
+console.log("1. Rezulatatul este egal cu: " + rez1)
+
+//2.  4 x (2 + 6/3)
+
+const rez2 = ( 4 * (2 + 6 / 3));
+console.log("2. Rezultatul este egal cu: " + rez2)
+
+//3.  2 x 3^2 + 2^5
+
+const rez3 =  (2 * 3**2 + 2**5);
+console.log ("3. Rezultatul este egal cu: " + rez3)
+
+//4.  [5 x (3 + 2/2 )] + 3
+
+const rez4 = ((5 * (3 + 2 / 2))  + 3);
+console.log ("4. Rezultatul este egal cu: " + rez4);
+
+//5.  [2**5 - 5**2 + 3 * (5 - 4/2)] / 2**5 + 5**2
+
+const rez5 = ((2**5 - 5**2 + 3 * (5 - 4/2)) / (2**5 + 5**2));
+console.log ("5. Rezultatul este egal cu: " + rez5);
---
+++ pb2.js
@@ -0,0 +1,12 @@
+let x = 5;
+let y = 8;
+
+//1.  2x-5y
+
+const ex1 = (2 * x - 5 * y);
+console.log ("Solutia ecuatiei nr.1 este: " + ex1)
+
+//2. 3x - 9y
+
+const ex2 = (3 * x + 9 * y);
+console.log ("Solutia ecuatiei nr.2 este: " + ex2)
